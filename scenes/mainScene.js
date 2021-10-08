8bb0cd23e551b961224544285b9417fd01519deb
drinkingWater <aamuti64@gmail.com>
2021-10-08 22:27:43 +0600

    adding player

--- scenes/mainScene.js
+++ scenes/mainScene.js
@@ -22,8 +22,7 @@ class MainScene extends Scene {
 
 
         this.#player = new Box({ name: 'box', props: { x: 900, y: 410, width: 32, height: 32 } });
-        // this.#player = new Player({ name: 'Ash' });
-        //     this.addEntity({ layer: 'players', entity: this.#player });
+        this.addEntity({ layer: 'Objects', entity: this.#player });
         this.#player.transform = {
             x: 1024,
             y: 416,
