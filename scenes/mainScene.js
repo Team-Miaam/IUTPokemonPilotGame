28fcbf3fc7e193dc424b3defd6092f68600d119c
Noman5237 <anonyman637@gmail.com>
2021-10-09 23:57:18 +0600

    entry state animation changed

--- scenes/mainScene.js
+++ scenes/mainScene.js
@@ -40,7 +40,7 @@ class MainScene extends Scene {
 		// this.#player = new Box({ name: 'box', props: { x: 900, y: 410, width: 32, height: 32 } });
 		this.#player = new Player({ name: 'player' });
 		this.addEntity({ layer: 'Objects', entity: this.#player });
-		this.#player.sprite.state = { state: 'idleUp' };
+		this.#player.sprite.state = { state: 'idleRight' };
 		// this.noman = new Noman({ name: 'noman' });
 		// this.addEntity({ layer: 'NPC', entity: this.noman });
 		this.prof = new Prof({ name: 'prof' });
